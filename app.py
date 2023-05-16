82d03cbe0c1be6be84ef02c593264d03c936a868
vladarik <swssb@bk.ru>
2023-05-16 19:59:30 +0700

    fixes to run in VM

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ app = create_app(Config())
 app.debug = True
 #
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    # app.run(host="localhost", port=5000, debug=True)
+    app.run()
